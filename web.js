70c2ea8eab3ca02f83ff591a50f0540b1b67cfab
jpmarindiaz <jpmarindiaz@gmail.com>
2013-07-18 13:44:13 -0300

    Read static html working

--- web.js
+++ web.js
@@ -1,15 +1,21 @@
 
+
 var express = require('express');
 var fs = require('fs');
 
 var index = fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
+
+buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+
 app.get('/', function(request, response) {
-  response.send(index);
+	response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
